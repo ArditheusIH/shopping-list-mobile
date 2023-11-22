2e689cd8060eff35b78da2b445054101e16b4430
ArditheusIH <arditheus.immanuel@gmail.com>
2023-11-22 12:58:11 +0700

    tutor 8 selesai

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 // Impor drawer widget
 import 'package:shopping_list/widgets/left_drawer.dart';
-import 'package:shopping_list/screens/shoplist_form.dart';
 import 'package:shopping_list/widgets/shop_cart.dart';
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
